005500b38f47f2fe170dc3fa6246bdfa0a15d1d7
Narender Gupta <narender.gupta01@gmail.com>
2015-02-09 17:37:04 -0600

    Implemented lazy file reader for csv dataset

---
+++ datamodel.py
@@ -0,0 +1,47 @@
+import csv
+import sys
+import statistics as stat
+
+
+class DataModel:
+  """Class that parses raw data"""
+  def __init__(self):
+    self.data_files = ['data/data1.csv', 'data/data2.csv']
+    self.readers = (
+        csv.reader(open('data/data1.csv', 'r'), delimiter=','),\
+        csv.reader(open('data/data2.csv', 'r'), delimiter=','),
+        )
+    self.data = []
+    self.headers = next(self.readers[0])
+
+
+  def get_header_index(self, header_name):
+    for i in range(len(self.headers)):
+      if self.headers[i] == header_name.lower().strip():
+        return i;
+    return -1
+
+
+  def get_header_name(self, header_index):
+    if header_index in range(len(self.headers)):
+      return self.headers[header_index]
+    return None
+
+
+  def get_data(self, num):
+    req_num = num
+    data_count = len(self.data)
+    if data_count < num:
+      ri = 0
+      while ri in range(len(self.readers)):
+        if num <= 0:
+          break
+        num = num + 1 - self.readers[ri].line_num
+        while num > 0:
+          try:
+            row = next(self.readers[ri])
+            self.data.append(row)
+            num = num - 1
+          except StopIteration as e:
+            ri = ri + 1
+    return self.data[:req_num]
--- parse_data.py
+++ parse_data.py
@@ -1,16 +1,15 @@
 import sys
-import csv
+from datamodel import DataModel
 
 
 def main(argv):
   train_count = -1
   if(len(argv)>0):
     train_count = int(argv[0])
-  fhandle = open('data/data1.csv', 'r')
-  freader = csv.reader(fhandle, delimiter=',')
-  for i in range(train_count):
-    print(next(freader))
-  return None
+  dm = DataModel()
+  lines = dm.get_data(train_count)
+  for i in range(len(lines)):
+    print(lines[i])
 
 
 if  __name__ == '__main__':
